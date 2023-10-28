1805afb3810a436896914d542e42d9ac2b02ac02
SimplyJuly <91342574+SimplyJuly@users.noreply.github.com>
2023-10-28 14:25:51 -0500

    Education Page
    I finally finished out the education page

--- src/components/CardDisplay.vue
+++ src/components/CardDisplay.vue
@@ -1,81 +1,84 @@
 <script setup>
 const props = defineProps ({
-  cardData: Array,
-  message: String
+  cardData: Array
 });
-
-// function toggle(data){
-//   console.log(data);
-//   data.toggle = !data.toggle;
-//   console.log(data);
-// }
-
 </script>
 
 <template>
-  <v-row class="padding">
-    <v-col v-for="data in cardData" :key="data.title">
-
-      <v-card class="cardSize">
-        <div v-if="!data.toggle">
-          <div class="imgContainer">
-            <img class="cardImg" :src="data.img">
+  <v-container>
+    <v-row class="padding">
+      <v-col v-for="data in cardData" :key="data.title">
+  
+        <!-- Default Card -->
+        <v-card class="cardSize">
+          <div v-if="!data.toggle">
+            <div class="imgContainer">
+              <img class="cardImg" :src="data.img">
+            </div>
+            <v-card-title>{{ data.title }}</v-card-title>
+            <v-card-subtitle>{{ data.time }}</v-card-subtitle>
+            <v-card-actions>
+              <v-btn color="teal-accent-4" @click="data.toggle = true">
+                Learn More
+              </v-btn>
+            </v-card-actions>
           </div>
-          <v-card-title>{{ data.title }}</v-card-title>
-          <v-card-subtitle class="subtitleText">{{ data.time }}</v-card-subtitle>
-          <v-card-actions>
-            <v-btn color="teal-accent-4" @click="data.toggle = true">
-              Learn More
-            </v-btn>
-          </v-card-actions>
-        </div>
+  
+          <!-- Learn More Card -->
+          <v-expand-transition>
+            <v-card v-if="data.toggle" class="revealedCard">
+              <v-row>
+                <v-col class="titleWrapper">
+                  <v-card-title>{{ data.title }}</v-card-title>
+                </v-col>
+                <v-col cols="12" style="height: 150px">
+                  <v-card-text>{{ data.description }}</v-card-text>
+                </v-col>
+                <v-col class="buttonWrapper">
+                  <v-card-actions>
+                    <v-btn 
+                      @click="data.toggle = false"
+                      class="revealButton">
+                      Return
+                    </v-btn>
+                  </v-card-actions>
+                </v-col>
+              </v-row>
+            </v-card>
+          </v-expand-transition>
 
-        <!-- Expanded version -->
-        <v-expand-transition>
-          <v-card v-if="data.toggle" class="revealedCard">
-
-            <v-row>
-              <v-col class="titleWrapper">
-                <v-card-title class="cardTitle">{{ data.title }}</v-card-title>
-              </v-col>
-              <v-col cols="12" style="height: 150px">
-                <v-card-text class="cardText">{{ data.description }}</v-card-text>
-              </v-col>
-              <v-col class="buttonWrapper">
-                <v-btn 
-                  @click="data.toggle = false"
-                  class="revealButton">
-                  Return
-                </v-btn>
-              </v-col>
-            </v-row>
-          </v-card>
-        </v-expand-transition>
-      </v-card>
-    </v-col>
-  </v-row>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template> 
 
 <style>
+/* Container Styling */
 .padding {
   padding: 30px;
 }
+
+/* Default Card Styling */
 .cardImg {
   max-height: 180px;
+  overflow: hidden;
   max-width: 300px;
   margin: auto;
 }
 .cardSize {
   width: 300px;
-  max-height: 300px;
+  height: 300px;
   z-index: 1;
 }
 .imgContainer {
   height: 180px;
-  display:flex;
+  width: 300px;
+  display: flex;
 }
 .v-card-subtitle {
   font-size: 1rem !important;
+  height: 20px
 }
 .button {
   box-shadow: 0px 3px 1px -2px var(--v-shadow-key-umbra-opacity, rgba(0, 0, 0, 0.2)), 0px 2px 2px 0px var(--v-shadow-key-penumbra-opacity, rgba(0, 0, 0, 0.14)), 0px 1px 5px 0px var(--v-shadow-key-penumbra-opacity, rgba(0, 0, 0, 0.12))
--- src/views/Experience.vue
+++ src/views/Experience.vue
@@ -1,47 +1,62 @@
 <script setup>
 import CardDisplay from "../components/CardDisplay.vue";
-import { ref } from 'vue'
+import { ref } from "vue";
 
-const experiences = ref([	
-		{
-			img: '/src/assets/CommerceLogo.png',
-			title: 'Software Engineer Intern',
-			time: '2023 - Present',
-			description: 'Full Stack Development in Blah blah. He is the bee\'s knees and does all of the cool stuff or something.',
-			toggle: false,
-		},
-		{
-			img: '/src/assets/ApproachLogo.jpeg',
-			title: 'Quality Assurance',
-			time: '2021 - 2023',
-			toggle: false,
-		},
-		{
-			img: '/src/assets/CommerceLogo.png',
-			title: 'Key Holder',
-			time: '2020 - Present',
-			toggle: false,
-		},
-		{
-			img: '/src/assets/CommerceLogo.png',
-			title: 'Key Holder',
-			time: '2020 - Present',
-			toggle: false,
-		},
-		{
-			img: '/src/assets/CommerceLogo.png',
-			title: 'Software Engineer Intern',
-			toggle: false,
-		},
-	]);
-
-const message = 'Hello'
+const experiences = ref([
+  {
+    img: "/CommerceLogo.png",
+    title: "Software Engineer Intern",
+    time: "2023 - Present",
+    description:
+      "Analyzing, designing, and maintaining a full-stack software application as part of an agile development team",
+    toggle: false,
+  },
+  {
+    img: "/approachLogo.png",
+    title: "Quality Assurance",
+    time: "2021 - 2023",
+    description:
+      "Identified bugs and problems in software applications, supported clients through technical issues.",
+    toggle: false,
+  },
+  {
+    img: "/RokcLogo.png",
+    title: "Front Desk",
+    time: "2020 - Present",
+    description:
+      "Opened and maintained climbing facility. Evaluated risk management for all climbers.",
+    toggle: false,
+  },
+  {
+    img: "/PizzahutLogo.jpeg",
+    title: "Shift Lead",
+    time: "2019 - 2020",
+    description:
+      "Ran store, enforced health regulations, managed delivery times.",
+    toggle: false,
+  },
+  {
+    img: "/KohlsLogo.png",
+    title: "Cashier",
+    time: "2018 - 2019",
+    description:
+      "Would check out customers and mantained surrounding clothing area.",
+    toggle: false,
+  },
+  {
+    img: "/AeropostaleLogo.png",
+    title: "Sales Associate",
+    time: "2017 - 2018",
+    description: "Helped customers find clothing, maintained store displays.",
+    toggle: false,
+  },
+]);
 </script>
 
 <template>
   <v-row class="shiftRight">
     <v-col>
-      <CardDisplay :cardData="experiences" :message="message"/>
+      <CardDisplay :cardData="experiences" />
     </v-col>
   </v-row>
 </template>
